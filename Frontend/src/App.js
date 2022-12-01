75701c7363934d433b02d7b2077df4ffb0539f94
sleepielle <pggisselle@gmail.com>
2022-12-01 16:55:03 -0600

    feat :; homepage
    homepage design is done and fully responsive.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,35 +1,44 @@
-
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Common/navbar";
 import Container from "react-bootstrap/Container";
-import Navbar from './Components/Common/navbar';
+import Button from "react-bootstrap/Button";
 import Card from './Components/Card/card';
 /*
 
 //componente propio de bootstrap
 import Navbar from './Components/Common/navbar';
-import Home from './Components/Home/Home';
+import Home from './Components/Home/Home';import Container from "react-bootstrap/Container";import Card from './Components/Card/card';
 */
 
-
-
 function App() {
-    return (
-      <div className="App">
-        <div className="App-header">
-        <Navbar/>
-            <Container className="cardParent">
-
-              <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card /> <Card />
-            </Container>
-        
-       
-        </div>
+  return (
+    
+     <div className="landingPic">
+    <Navbar className="nav"/>
+       <div className="heroImage">
+         <p className="allrightFont">delika</p>
+         <p className="kionaFont">Stylish living does not have </p>
+         <p className="kionaFont2">to be expensive to be classy. </p>
+       <Button className="kionaFontBtn" variant="outline-warning">
+           Register
+        </Button>{" "}
       </div>
-    );
-  }
-
-
+      </div>
+  );
+}
 
 export default App;
 
+
+
+//card demo
+  //   return (
+    
+  //     <div className="landingPic">
+  //   <Navbar className="nav"/>
+  //   <div className="cardParent">
+  //      <Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/>
+  //      </div>
+  //    </div>
+  //  );
