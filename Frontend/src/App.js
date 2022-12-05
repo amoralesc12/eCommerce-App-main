9ab5b80697569177e7a1529b27fc520b6caf41fe
sleepielle <pggisselle@gmail.com>
2022-12-04 21:28:30 -0600

    frontend login

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,44 +1,16 @@
-import React from "react";
-import "./App.css";
-import Navbar from "./Components/Common/navbar";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
-import Card from './Components/Card/card';
-/*
 
-//componente propio de bootstrap
+import React from 'react';
+import './App.css';
+
 import Navbar from './Components/Common/navbar';
-import Home from './Components/Home/Home';import Container from "react-bootstrap/Container";import Card from './Components/Card/card';
-*/
 
 function App() {
   return (
-    
-     <div className="landingPic">
-    <Navbar className="nav"/>
-       <div className="heroImage">
-         <p className="allrightFont">delika</p>
-         <p className="kionaFont">Stylish living does not have </p>
-         <p className="kionaFont2">to be expensive to be classy. </p>
-       <Button className="kionaFontBtn" variant="outline-warning">
-           Register
-        </Button>{" "}
-      </div>
-      </div>
+    <div className="App">
+
+       <Navbar/>
+    </div>
   );
 }
 
 export default App;
-
-
-
-//card demo
-  //   return (
-    
-  //     <div className="landingPic">
-  //   <Navbar className="nav"/>
-  //   <div className="cardParent">
-  //      <Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/>
-  //      </div>
-  //    </div>
-  //  );
--- Frontend/src/Components/Common/navbar.jsx
+++ Frontend/src/Components/Common/navbar.jsx
@@ -1,50 +1,27 @@
-import React from "react";
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import "./navbar.css";
-import Button from 'react-bootstrap/Button';
+
+
+
 function myNavBar(props) {
-  
-  return (
-
-
-    <Navbar className="responsive" expand="lg">
-    <Container  fluid>
-      <Navbar.Brand className="title" href="#">Delika.</Navbar.Brand>
-      <Navbar.Collapse id="navbarScroll">
-      
-        <Nav
-          className="extend"
-          navbarScroll
-        >
-          
-           
-          <Nav.Link className="home" href="/home">Home</Nav.Link>
-          <Nav.Link className="about" href="/about">About</Nav.Link>
-          <Nav.Link className="contact" href="/contact">Contact</Nav.Link>
-          <Button className="kionaFontButton" variant="outline-warning">Log In</Button>{' '}
-        </Nav>
-      
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  /*
-    <Navbar expand="lg">
-      <Container className="topPadding">
-        <Navbar.Brand  href="#home">Delika.</Navbar.Brand>
+    return (
+        <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="/home">Home</Nav.Link>
               <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
+                <Nav.Link href="/contact">Contact</Nav.Link>
+            
             </Nav>
           </Navbar.Collapse>
-       
-      </Container>
-    </Navbar>*/
-  );
+        </Container>
+      </Navbar>
+    );
 }
 
 export default myNavBar;
